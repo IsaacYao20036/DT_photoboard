20290e8d96f781fefba35bc3bd1bbd8d6888d38c
IsaacYao20036 <20036@burnside.school.nz>
2024-06-27 21:36:52 +1200

    filter dropdown on searchpg works properly

--- app/static/js/search.js
+++ app/static/js/search.js
@@ -4,9 +4,11 @@ function changeListener(){
     var value = this.value
 
     if (value == "fname"){
-        document.getElementById("fnameSorted").style.display = "none";
-    } else if (value == "lname"){
+        document.getElementById("fnameSorted").style.display = "grid";
         document.getElementById("lnameSorted").style.display = "none";
+    } else if (value == "lname"){
+        document.getElementById("lnameSorted").style.display = "grid";
+        document.getElementById("fnameSorted").style.display = "none";
     }
 
 }
