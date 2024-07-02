906bf166c105dfcd3cc146f359483ca71dccbf2a
IsaacYao20036 <20036@burnside.school.nz>
2024-07-02 21:44:48 +1200

    working alphabet links for searchpg

--- app/static/js/search.js
+++ app/static/js/search.js
@@ -4,9 +4,13 @@ function changeListener() {
     let value = this.value
 
     if (value == "fname") {
+        document.getElementById("alphabetLinks_f").style.display = "block"
+        document.getElementById("alphabetLinks_l").style.display = "none"
         document.getElementById("allCards_f").style.display = "block"
         document.getElementById("allCards_l").style.display = "none"
     } else if (value == "lname") {
+        document.getElementById("alphabetLinks_f").style.display = "none"
+        document.getElementById("alphabetLinks_l").style.display = "block"
         document.getElementById("allCards_f").style.display = "none"
         document.getElementById("allCards_l").style.display = "block"
     }
